34f41f2a86845490fea2f46092f1dea14e2ebeee
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2022-08-31 08:22:23 -0700

    Finish nav bar.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -63,6 +63,7 @@ const Navbar = () => {
   const { theme, setTheme } = useTheme();
   const [active, setActive] = useState('Explore NFTs');
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState(true);
   console.log({ theme });
   return (
     <nav className="flexBetween w-full fixed
@@ -130,7 +131,40 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-      <div />
+      <div className="hidden md:flex ml-2">
+        {isOpen
+          ? (
+            <Image
+              src={images.cross}
+              objectFit="contain"
+              width={20}
+              height={20}
+              alt="close"
+              onClick={() => setIsOpen(false)}
+              className={theme === 'light' && 'filter invert'}
+            />
+          ) : (
+            <Image
+              src={images.menu}
+              objectFit="contain"
+              width={25}
+              height={25}
+              alt="menu"
+              onClick={() => setIsOpen(true)}
+              className={theme === 'light' && 'filter invert'}
+            />
+          )}
+        {isOpen && (
+        <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col">
+          <div className="flex-1 p-4">
+            <MenuItems active={active} setActive={setActive} isMobile />
+          </div>
+          <div className="p-4 border-t dark:border-nft-black-1 border-nft-gray-1">
+            <ButtonGroup setActive={setActive} router={router} />
+          </div>
+        </div>
+        )}
+      </div>
     </nav>
   );
 };
