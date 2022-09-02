698faada29be61b8a385bb2d34a03f6b5fb2e0e1
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2022-09-02 07:16:35 -0700

    Add scroll functionality to top sellers page.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,48 @@
 import { useEffect, useState, useRef, useContext } from 'react';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
 import { Banner, CreatorCard } from '../components';
 import images from '../assets';
 import { makeId } from '../utils/makeId';
 
 const Home = () => {
+  const { theme } = useTheme();
   const parentRef = useRef(null);
   const scrollRef = useRef();
+  const [hideButtons, setHideButtons] = useState(false);
+
+  const handleScroll = (direction) => {
+    const { current } = scrollRef;
+
+    const scrollAmount = window.innerWidth > 1800 ? 270 : 210;
+
+    if (direction === 'left') {
+      current.scrollLeft -= scrollAmount;
+    } else {
+      current.scrollLeft += scrollAmount;
+    }
+  };
+
+  const isScrollable = () => {
+    const { current } = scrollRef;
+    const { current: parent } = parentRef;
+
+    if (current?.scrollWidth >= parent?.offsetWidth) {
+      setHideButtons(false);
+    } else {
+      setHideButtons(true);
+    }
+  };
+
+  useEffect(() => {
+    isScrollable();
+    window.addEventListener('resize', isScrollable);
+
+    return () => {
+      window.removeEventListener('resize', isScrollable);
+    };
+  });
+
   console.log(makeId(3));
   return (
     <div className="flex justify-center sm:px-4 p-12">
@@ -28,6 +65,16 @@ const Home = () => {
                   creatorEths={10 - i * 0.5}
                 />
               ))}
+              {!hideButtons && (
+              <>
+                <div onClick={() => handleScroll('left')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0">
+                  <Image src={images.left} layout="fill" objectFit="contain" alt="left_arrow" className={theme === 'light' ? 'filter invert' : undefined} />
+                </div>
+                <div onClick={() => handleScroll('right')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0">
+                  <Image src={images.right} layout="fill" objectFit="contain" alt="left_arrow" className={theme === 'light' ? 'filter invert' : undefined} />
+                </div>
+              </>
+              )}
             </div>
           </div>
         </div>
