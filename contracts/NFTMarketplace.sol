b45ae43575eb7051abeb10013e9c076492dac593
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2022-09-04 20:26:06 -0700

    Fetch items listed by the user.

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -145,22 +145,89 @@ contract NFTMarketplace is ERC721URIStorage {
       payable(idToMarketItem[tokenId].seller).transfer(msg.value);
     }
 
+    /* Returns all unsold market items */
     function fetchMarketItems() public view returns (MarketItem[] memory) {
-        uint itemCount = _tokenIds.current();
-        uint unsoldItemCount = _tokenIds.current() - _itemsSold.current();
-        uint currentIndex = 0;
+      uint itemCount = _tokenIds.current();
+      uint unsoldItemCount = _tokenIds.current() - _itemsSold.current();
+      uint currentIndex = 0;
 
-        MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+      // looping over the number of items created and incremnet htat number if we have an empty address 
 
-        for (uint i = 0; i < itemCount; i ++) {
-            if (idToMarketItem[i + 1].owner == address(this)) {
-                uint currentId = i + 1;
-                MarketItem storage currentItem = idToMarketItem[currentId];
-                items[currentIndex] = currentItem;
-                currentIndex += 1;
-            }
+      // empty array called items
+      // the type of the element in the array is marketitem, and the unsolditemcount is the lenght
+      MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+      for (uint i = 0; i < itemCount; i++) {
+        // check to see if the item is unsold -> checking if the owner is an empty address -> then it's unsold
+        // above, where we were creating a new market item, we were setting the address to be an empty address
+        // the address get's populated if the item is sold
+        if (idToMarketItem[i + 1].owner == address(this)) {
+          // the id of the item that we're currently interracting with
+          uint currentId = i + 1;
+          // get the mapping of the idtomarketitem with the -> gives us the reference to the marketitem
+          MarketItem storage currentItem = idToMarketItem[currentId];
+          // insert the market item to the items array
+          items[currentIndex] = currentItem;
+          // increment the current index
+          currentIndex += 1;
         }
+      }
 
-        return items;
+      return items;
+    }
+
+    /* Returns only items that a user has purchased */
+    function fetchMyNFTs() public view returns (MarketItem[] memory) {
+      uint totalItemCount = _tokenIds.current();
+      uint itemCount = 0;
+      uint currentIndex = 0;
+
+      // gives us the number of items that we own
+      for (uint i = 0; i < totalItemCount; i++) {
+        // check if nft is mine
+        if (idToMarketItem[i + 1].owner == msg.sender) {
+          itemCount += 1;
+        }
+      }
+
+      MarketItem[] memory items = new MarketItem[](itemCount);
+      for (uint i = 0; i < totalItemCount; i++) {
+        // check if nft is mine
+        if (idToMarketItem[i + 1].owner == msg.sender) {
+          // get the id of the market item
+          uint currentId = i + 1;
+          // get the reference to the current market item
+          MarketItem storage currentItem = idToMarketItem[currentId];
+          // insert into the array
+          items[currentIndex] = currentItem;
+          // increment the index
+          currentIndex += 1;
+        }
+      }
+      return items;
+    }
+
+    /* Returns only items a user has listed */
+    function fetchItemsListed() public view returns (MarketItem[] memory) {
+      uint totalItemCount = _tokenIds.current();
+      uint itemCount = 0;
+      uint currentIndex = 0;
+
+      for (uint i = 0; i < totalItemCount; i++) {
+        if (idToMarketItem[i + 1].seller == msg.sender) {
+          itemCount += 1;
+        }
+      }
+
+      MarketItem[] memory items = new MarketItem[](itemCount);
+      for (uint i = 0; i < totalItemCount; i++) {
+        if (idToMarketItem[i + 1].seller == msg.sender) {
+          uint currentId = i + 1;
+          MarketItem storage currentItem = idToMarketItem[currentId];
+          items[currentIndex] = currentItem;
+          currentIndex += 1;
+        }
+      }
+      
+      return items;
     }
 }
