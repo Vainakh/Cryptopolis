36a8d422f7870a4208386cdef51d5368fb933d2a
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2022-09-18 18:22:43 -0700

    Finish listed nfts page.

--- pages/listed-nfts.js
+++ pages/listed-nfts.js
@@ -3,8 +3,17 @@ import { NFTContext } from '../context/NFTContext';
 import { NFTCard, Loader } from '../components';
 
 const ListedNFTs = () => {
+  const { fetchMyNFTsOrListedNFTs } = useContext(NFTContext);
   const [nfts, setNfts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchMyNFTsOrListedNFTs('fetchItemsListed')
+      .then((items) => {
+        setNfts(items);
+        setIsLoading(false);
+      });
+  }, []);
 
   if (isLoading) {
     return (
@@ -13,9 +22,30 @@ const ListedNFTs = () => {
       </div>
     );
   }
+
+  // if (!isLoading && nfts.length === 0) {
+  //   return (
+  //     <div className="flexCenter sm:p-4 p-16 min-h-screen">
+  //       <h1 className="font-poppins dark:text-white text-nft-black-1 text-3xl font-extrabold">
+  //         No NFTs listed for sale!
+  //         ...and that is funny!
+  //       </h1>
+  //     </div>
+  //   );
+  // }
+
   return (
-    <div>
-      ListedNFTs
+    <div className="flex justify-content sm:px-4 p-12 min-h-screen">
+      <div className="w-full minmd:w-4/5">
+        <div className="mt-4">
+          <h2 className="font-poppins dark:text-white text-nft-black-1 text-2xl font-semibold mt-2 ml-4 sm:ml-2">
+            NFTs listed for sale!
+          </h2>
+          <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
+            { nfts.map((nft) => <NFTCard key={nft.tokenId} nft={nft} />)}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
