8611fb1265b447777ce8e18f8b40aee56b306b7e
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2022-09-19 19:32:37 -0700

    Create Payment body component.

--- pages/nft-details.js
+++ pages/nft-details.js
@@ -7,8 +7,33 @@ import { NFTCard, Loader, Button, Modal } from '../components';
 import { shortenAddress } from '../utils/shortenAddress';
 
 const PaymentBodyCmp = ({ nft, nftCurrency }) => (
-  <div>
-    Test
+  <div className="flex flex-col">
+    <div className="flexBetween">
+      <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base minlg:text-xl">Item</p>
+      <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base minlg:text-xl">Subtotal</p>
+    </div>
+
+    <div className="flexBetweenStart my-5">
+      <div className="flex-1 flexStartCenter">
+        <div className="relative w-28 h-28">
+          {/* <Image src={nft.image || images[`nft${nft.i}`]} layout="fill" objectFit="cover" /> */}
+          <img src={nft.image || images[`nft${nft.i}`]} />
+        </div>
+        <div className="flexCenterStart flex-col ml-5">
+          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm minlg:text-xl">{shortenAddress(nft.seller)}</p>
+          <p className="font-poppins dark:text-white text-nft-black-1 text-sm minlg:text-xl font-normal">{nft.name}</p>
+        </div>
+      </div>
+
+      <div>
+        <p className="font-poppins dark:text-white text-nft-black-1 text-sm minlg:text-xl font-normal">{nft.price} <span className="font-semibold">{nftCurrency}</span></p>
+      </div>
+    </div>
+
+    <div className="flexBetween mt-10">
+      <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base minlg:text-xl">Total</p>
+      <p className="font-poppins dark:text-white text-nft-black-1 text-base minlg:text-xl font-normal">{nft.price} <span className="font-semibold">{nftCurrency}</span></p>
+    </div>
   </div>
 );
 
